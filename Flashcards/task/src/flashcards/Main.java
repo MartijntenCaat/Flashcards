d2bd0782526eaec278e6a42a335d3a7ab7d0e48a
Martijn ten Caat <tencaat@gmail.com>
2020-04-14 15:14:47 +0200

    Feedback verwerkt.

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -2,30 +2,31 @@ package flashcards;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     private LinkedHashMap<String, String> cardMap;
-    private Scanner scanner;
-    private boolean gameIsActive;
+    private Scanner userScanner;
+    private boolean isUpAndRunning;
 
     private Main() {
-        this.scanner = new Scanner(System.in);
+        this.userScanner = new Scanner(System.in);
         this.cardMap = new LinkedHashMap<>();
-        this.gameIsActive = true;
+        this.isUpAndRunning = true;
     }
 
     private void exitGame() {
-        gameIsActive = false;
-        scanner.close();
+        isUpAndRunning = false;
+        userScanner.close();
         System.out.println("Bye bye!");
     }
 
     private void runGameByAction() {
         System.out.println("Input the action (add, remove, import, export, ask, exit):");
-        String action = scanner.nextLine();
+        String action = userScanner.nextLine();
 
         switch (action) {
             case "add":
@@ -53,7 +54,7 @@ public class Main {
 
     private void removeCards() {
         System.out.println("The card:");
-        String cardToBeRemoved = scanner.nextLine();
+        String cardToBeRemoved = userScanner.nextLine();
 
         if (cardMap.containsKey(cardToBeRemoved)) {
             cardMap.remove(cardToBeRemoved);
@@ -65,102 +66,123 @@ public class Main {
 
     private void importCards() {
         System.out.println("File name:");
-        File file = new File(scanner.nextLine());
+        File file = new File(userScanner.nextLine());
 
         int cardsImported = 0;
 
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNext()) {
-                String cardQuestion = scanner.nextLine();
-                String cardDefinition = scanner.nextLine();
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNext()) {
+                String cardQuestion = fileScanner.nextLine();
+                String cardDefinition = fileScanner.nextLine();
 
                 cardMap.put(cardQuestion, cardDefinition);
                 cardsImported++;
             }
             System.out.println(cardsImported + " cards have been loaded.");
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("File not found.");
         }
     }
 
     private void exportCards() {
         System.out.println("File name:");
-        File file = new File(scanner.nextLine());
+        File file = new File(userScanner.nextLine());
 
         int numberOfSavedCards = 0;
 
         try (FileWriter cardWriter = new FileWriter(file, false)) {
             for (String cardQuestion : cardMap.keySet()) {
-                String exportLine = cardQuestion + "\n" + cardMap.get(cardQuestion) + "\n";
-                cardWriter.write(exportLine);
+                cardWriter.write(cardQuestion + "\n");
+                cardWriter.write(cardMap.get(cardQuestion) + "\n");
                 numberOfSavedCards++;
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Something went wrong: \n" + e);
         }
         System.out.println(numberOfSavedCards + " cards have been saved.");
     }
 
     private void fillCardMap() {
+        String cardQuestion;
+        String cardDefinition = null;
+
         System.out.println("The card:");
-        String cardQuestion = scanner.nextLine();
+        cardQuestion = userScanner.nextLine();
 
         if (cardMap.containsKey(cardQuestion)) {
             System.out.println("The card \"" + cardQuestion + "\" already exists.");
-        } else {
-            System.out.println("The definition of the card:");
-            String cardDefinition = scanner.nextLine();
+        }
 
-            if (cardMap.containsValue(cardDefinition)) {
-                System.out.println("The definition \"" + cardDefinition + "\" already exists.");
-            } else {
-                cardMap.put(cardQuestion, cardDefinition);
-                System.out.println("The pair (\"" + cardQuestion + "\"" + ":" + "\"" + cardDefinition + "\") has been added.");
-            }
+        if (!cardMap.containsKey(cardQuestion)) {
+            System.out.println("The definition of the card:");
+            cardDefinition = userScanner.nextLine();
+        }
+
+        if (!cardMap.containsKey(cardQuestion) && cardMap.containsValue(cardDefinition)) {
+            System.out.println("The definition \"" + cardDefinition + "\" already exists.");
+        }
+
+        if (!cardMap.containsKey(cardQuestion) && !cardMap.containsValue(cardDefinition)) {
+            cardMap.put(cardQuestion, cardDefinition);
+            System.out.println("The pair (\"" + cardQuestion + "\"" + ":" + "\"" + cardDefinition + "\") has been added.");
         }
     }
 
     private String getRightCardQuestion(String answer) {
-        String rightCardQuestion = null;
-        for (String CardQuestionToFins : cardMap.keySet()) {
-            if (answer.equals(cardMap.get(CardQuestionToFins))) {
-                rightCardQuestion = CardQuestionToFins;
-                break;
+        for (String cardQuestionToFind : cardMap.keySet()) {
+            if (answer.equals(cardMap.get(cardQuestionToFind))) {
+                return cardQuestionToFind;
             }
         }
-        return rightCardQuestion;
+        return null;
+    }
+
+    private void checkAnswer(String cardQuestion, String cardAnswer) {
+        if (cardMap.get(cardQuestion).equals(cardAnswer)) {
+            System.out.println("Correct answer.");
+        }
+
+        if (cardMap.containsValue(cardAnswer) && !cardMap.get(cardQuestion).equals(cardAnswer)) {
+            System.out.println("Wrong answer. The correct one is \"" + cardMap.get(cardQuestion) + "\", " +
+                    "you've just written the definition of \"" + getRightCardQuestion(cardAnswer) + "\"");
+        }
+
+        if (!cardMap.containsValue(cardAnswer)) {
+            System.out.println("Wrong answer. The correct one is \"" + cardMap.get(cardQuestion) + "\".");
+        }
+    }
+
+    private int askDurationOfGame() {
+        System.out.println("How many times to ask?");
+        return Integer.parseInt(userScanner.nextLine());
+    }
+
+    private String[] prepareGame(int duration) {
+        Object[] allTheCardQuestions = cardMap.keySet().toArray();
+        Random randomised = new Random();
+        String[] cardQuestionList = new String[duration];
+
+        for (int i = 0; i < duration; i++) {
+            cardQuestionList[i] = allTheCardQuestions[randomised.nextInt(allTheCardQuestions.length)].toString();
+        }
+        return cardQuestionList;
     }
 
     private void runFlashCardGame() {
-        Random randomised = new Random();
+        int duration = askDurationOfGame();
+        String[] cardQuestionList = prepareGame(duration);
 
-        System.out.println("How many times to ask?");
-        int numberOfQuestions = Integer.parseInt(scanner.nextLine());
-
-        Object[] cardQuestionList = cardMap.keySet().toArray();
-
-        for (int i = 0; i < numberOfQuestions; i++) {
-            String cardQuestion = cardQuestionList[randomised.nextInt(cardQuestionList.length)].toString();
-
+        for (String cardQuestion : cardQuestionList) {
             System.out.println("Print the definition of \"" + cardQuestion + "\":");
-            String cardAnswerByPlayer = scanner.nextLine();
+            String cardAnswerByPlayer = userScanner.nextLine();
 
-            if (cardMap.containsValue(cardAnswerByPlayer)) {
-                if (cardMap.get(cardQuestion).equals(cardAnswerByPlayer)) {
-                    System.out.println("Correct answer.");
-                } else {
-                    System.out.println("Wrong answer. The correct one is \"" + cardMap.get(cardQuestion) + "\", " +
-                        "you've just written the definition of \"" + getRightCardQuestion(cardAnswerByPlayer) + "\"");
-                }
-            } else {
-                System.out.println("Wrong answer. The correct one is \"" + cardMap.get(cardQuestion) + "\".");
-            }
+            checkAnswer(cardQuestion, cardAnswerByPlayer);
         }
     }
 
     public static void main(String[] args) {
         Main main = new Main();
-        while (main.gameIsActive) {
+        while (main.isUpAndRunning) {
             main.runGameByAction();
         }
     }
