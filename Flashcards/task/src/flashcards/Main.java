4472b3d2a359005ae1bd70d49e0ddf7f82eea189
Martijn ten Caat <tencaat@gmail.com>
2020-04-28 16:15:31 +0200

    Removed import of LinkedHashMap

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.Random;
 import java.util.Scanner;
 
